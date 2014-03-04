8532435bcd9b4751dfdfe478b1132752eb512094
david.cifuentes <david.cifuentes@eforcers.com>
2014-03-03 19:34:33 -0500

    Adding appstats

--- course-google-app-engine/python/lesson5_remote/main.py
+++ course-google-app-engine/python/lesson5_remote/main.py
@@ -1,5 +1,15 @@
 import webapp2
 
-app = webapp2.WSGIApplication([
+from views import InsertHandler, InsertMultiHandler, ReadAllHandler, \
+    ReadAllKeysHandler, DeleteHandler, DeleteAllHandler, CallHookHandler
 
+app = webapp2.WSGIApplication([
+    ('/insert', InsertHandler),
+    ('/insert_multi',InsertMultiHandler),
+    ('/read_all', ReadAllHandler),
+    ('/read', ReadAllHandler),
+    ('/read_all_key', ReadAllKeysHandler),
+    ('/delete', DeleteHandler),
+    ('/delete_all', DeleteAllHandler),
+    ('/call_hook', CallHookHandler),
 ], debug=True)
